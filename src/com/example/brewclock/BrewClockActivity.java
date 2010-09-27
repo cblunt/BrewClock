e8930c44f6a30b91517aacbc554dde751e1f95a0
cblunt <chris@chrisblunt.com>
2010-09-27 14:52:25 +0100

    Added interface element properties

--- src/com/example/brewclock/BrewClockActivity.java
+++ src/com/example/brewclock/BrewClockActivity.java
@@ -2,12 +2,21 @@ package com.example.brewclock;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class BrewClockActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+  /** Properties **/
+  protected Button brewAddTime;
+  protected Button brewDecreaseTime;
+  protected Button startBrew;
+  protected TextView brewCountLabel;
+  protected TextView brewTimeLabel;
+  
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }
 }
