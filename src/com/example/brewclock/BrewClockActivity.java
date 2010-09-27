5765f0f2a988acc56a16de1da3ae18df4c8e3fa8
cblunt <chris@chrisblunt.com>
2010-09-27 13:33:01 +0100

    Initial Commit

---
+++ src/com/example/brewclock/BrewClockActivity.java
@@ -0,0 +1,13 @@
+package com.example.brewclock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BrewClockActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
