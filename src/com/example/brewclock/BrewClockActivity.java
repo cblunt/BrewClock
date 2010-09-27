20893e3a0fd458e7fd0992f952b95459087f6ca5
cblunt <chris@chrisblunt.com>
2010-09-27 15:45:07 +0100

    Basic brew timer working.

--- src/com/example/brewclock/BrewClockActivity.java
+++ src/com/example/brewclock/BrewClockActivity.java
@@ -2,6 +2,7 @@ package com.example.brewclock;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -15,6 +16,11 @@ public class BrewClockActivity extends Activity implements OnClickListener {
   protected TextView brewCountLabel;
   protected TextView brewTimeLabel;
 
+  protected int brewTime = 3;
+  protected CountDownTimer brewCountDownTimer;
+  protected int brewCount = 0;
+  protected boolean isBrewing = false;
+  
   /** Called when the activity is first created. */
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -27,13 +33,93 @@ public class BrewClockActivity extends Activity implements OnClickListener {
     startBrew = (Button) findViewById(R.id.brew_start);
     brewCountLabel = (TextView) findViewById(R.id.brew_count_label);
     brewTimeLabel = (TextView) findViewById(R.id.brew_time);
+    
+    // Setup ClickListeners
+    brewAddTime.setOnClickListener(this);
+    brewDecreaseTime.setOnClickListener(this);
+    startBrew.setOnClickListener(this);
+    
+    // Set the initial brew values
+    setBrewCount(0);
+    setBrewTime(3);
   }
-
+  
+  /** Methods **/
+  
+  /**
+   * Set an absolute value for the number of minutes to brew. Has no effect if a brew
+   * is currently running.
+   * @param minutes The number of minutes to brew.
+   */
+  public void setBrewTime(int minutes) {
+    if(isBrewing)
+      return;
+    
+    if(brewTime < 1)
+      brewTime = 1;
+    
+    brewTime = minutes;
+    brewTimeLabel.setText(String.valueOf(brewTime) + "m");
+  }
+  
+  /**
+   * Set the number of brews that have been made, and update the interface. 
+   * @param count The new number of brews
+   */
+  public void setBrewCount(int count) {
+    brewCount = count;
+    brewCountLabel.setText(String.valueOf(brewCount));
+  }
+  
+  /**
+   * Start the brew timer
+   */
+  public void startBrew() {
+    isBrewing = true;
+    
+    // Create a new CountDownTimer to track the brew time
+    brewCountDownTimer = new CountDownTimer(brewTime * 60 * 1000, 1000) {
+      @Override
+      public void onTick(long millisUntilFinished) {
+        brewTimeLabel.setText(String.valueOf(millisUntilFinished / 1000) + "s");
+      }
+      
+      @Override
+      public void onFinish() {
+        isBrewing = false;
+        setBrewCount(brewCount + 1);
+        
+        brewTimeLabel.setText("Brew Up!");
+      }
+    };
+    
+    brewCountDownTimer.start();
+  }
+  
+  /**
+   * Stop the brew timer
+   */
+  public void stopBrew() {
+    if(brewCountDownTimer != null)
+      brewCountDownTimer.cancel();
+    
+    isBrewing = false;
+  }
+  
   /** Interface Implementations **/
   /* (non-Javadoc)
    * @see android.view.View.OnClickListener#onClick(android.view.View)
    */
   public void onClick(View v) {
-    // TODO: Add code to handle button taps
+    if(v == brewAddTime)
+      setBrewTime(brewTime + 1);
+    else if(v == brewDecreaseTime)
+      setBrewTime(brewTime -1);
+    else if(v == startBrew) {
+      if(isBrewing)
+        stopBrew();
+      else
+        startBrew();
+    }
   }
 }
