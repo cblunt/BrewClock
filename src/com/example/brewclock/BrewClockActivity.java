10330e30c5997c67893a20bd5bc520aed81e0d47
cblunt <chris@chrisblunt.com>
2010-09-27 15:05:07 +0100

    Implement OnClickListener interface

--- src/com/example/brewclock/BrewClockActivity.java
+++ src/com/example/brewclock/BrewClockActivity.java
@@ -2,10 +2,12 @@ package com.example.brewclock;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class BrewClockActivity extends Activity {
+public class BrewClockActivity extends Activity implements OnClickListener {
   /** Properties **/
   protected Button brewAddTime;
   protected Button brewDecreaseTime;
@@ -25,5 +27,13 @@ public class BrewClockActivity extends Activity {
     startBrew = (Button) findViewById(R.id.brew_start);
     brewCountLabel = (TextView) findViewById(R.id.brew_count_label);
     brewTimeLabel = (TextView) findViewById(R.id.brew_time);
+  }
+
+  /** Interface Implementations **/
+  /* (non-Javadoc)
+   * @see android.view.View.OnClickListener#onClick(android.view.View)
+   */
+  public void onClick(View v) {
+    // TODO: Add code to handle button taps
   }
 }
