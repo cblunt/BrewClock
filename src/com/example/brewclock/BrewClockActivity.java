483b381b8baa0973195b097effdf448d5f1e60a4
Chris Blunt <chris@chrisblunt.com>
2010-11-05 11:22:57 +0000

    Add options menu handler.
      * Placeholder code to display the TeaManager activity.

--- src/com/example/brewclock/BrewClockActivity.java
+++ src/com/example/brewclock/BrewClockActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -22,7 +23,7 @@ public class BrewClockActivity extends Activity implements OnClickListener {
   protected CountDownTimer brewCountDownTimer;
   protected int brewCount = 0;
   protected boolean isBrewing = false;
-  
+
   /** Called when the activity is first created. */
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -35,32 +36,49 @@ public class BrewClockActivity extends Activity implements OnClickListener {
     startBrew = (Button) findViewById(R.id.brew_start);
     brewCountLabel = (TextView) findViewById(R.id.brew_count_label);
     brewTimeLabel = (TextView) findViewById(R.id.brew_time);
-    
+
     // Setup ClickListeners
     brewAddTime.setOnClickListener(this);
     brewDecreaseTime.setOnClickListener(this);
     startBrew.setOnClickListener(this);
-    
+
     // Set the initial brew values
     setBrewCount(0);
     setBrewTime(3);
   }
-  
+
   /** Methods **/
 
   /** Overrides **/
-  
-  /* (non-Javadoc)
+
+  /*
+   * (non-Javadoc)
    * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
    */
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     MenuInflater inflater = getMenuInflater();
     inflater.inflate(R.menu.main, menu);
-    
+
     return true;
   }
-  
+
+  /*
+   * (non-Javadoc)
+   * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
+   */
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch(item.getItemId()) {
+      case R.id.manage_teas:
+        // TODO Display the tea manager activity.
+        return true;
+
+      default:
+        return super.onOptionsItemSelected(item);
+    }
+  }
+
   /**
    * Set an absolute value for the number of minutes to brew. Has no effect if a brew
    * is currently running.
@@ -69,24 +87,24 @@ public class BrewClockActivity extends Activity implements OnClickListener {
   public void setBrewTime(int minutes) {
     if(isBrewing)
       return;
-    
+
     brewTime = minutes;
-    
+
     if(brewTime < 1)
       brewTime = 1;
 
     brewTimeLabel.setText(String.valueOf(brewTime) + "m");
   }
-  
+
   /**
-   * Set the number of brews that have been made, and update the interface. 
+   * Set the number of brews that have been made, and update the interface.
    * @param count The new number of brews
    */
   public void setBrewCount(int count) {
     brewCount = count;
     brewCountLabel.setText(String.valueOf(brewCount));
   }
-  
+
   /**
    * Start the brew timer
    */
@@ -97,42 +115,43 @@ public class BrewClockActivity extends Activity implements OnClickListener {
       public void onTick(long millisUntilFinished) {
         brewTimeLabel.setText(String.valueOf(millisUntilFinished / 1000) + "s");
       }
-      
+
       @Override
       public void onFinish() {
         isBrewing = false;
         setBrewCount(brewCount + 1);
-        
+
         brewTimeLabel.setText("Brew Up!");
         startBrew.setText("Start");
       }
     };
-    
+
     brewCountDownTimer.start();
     startBrew.setText("Stop");
     isBrewing = true;
   }
-  
+
   /**
    * Stop the brew timer
    */
   public void stopBrew() {
     if(brewCountDownTimer != null)
       brewCountDownTimer.cancel();
-    
+
     isBrewing = false;
     startBrew.setText("Start");
   }
-  
+
   /** Interface Implementations **/
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
    * @see android.view.View.OnClickListener#onClick(android.view.View)
    */
   public void onClick(View v) {
     if(v == brewAddTime)
       setBrewTime(brewTime + 1);
     else if(v == brewDecreaseTime)
-      setBrewTime(brewTime -1);
+      setBrewTime(brewTime - 1);
     else if(v == startBrew) {
       if(isBrewing)
         stopBrew();
