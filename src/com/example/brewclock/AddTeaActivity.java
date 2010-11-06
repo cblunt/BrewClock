fac7a18614e6667a211ec5b9b8713e76d9c55280
Chris Blunt <chris@chrisblunt.com>
2010-11-06 09:27:27 +0000

    Working brew time seekbar.

--- src/com/example/brewclock/AddTeaActivity.java
+++ src/com/example/brewclock/AddTeaActivity.java
@@ -2,8 +2,20 @@ package com.example.brewclock;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
-public class AddTeaActivity extends Activity {
+public class AddTeaActivity 
+  extends Activity 
+  implements OnSeekBarChangeListener {
+  
+  /** Properties **/
+  protected EditText teaName;
+  protected SeekBar brewTimeSeekBar;
+  protected TextView brewTimeLabel;
+
   /** Overrides **/
   
   /* (non-Javadoc)
@@ -13,5 +25,25 @@ public class AddTeaActivity extends Activity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.add_tea);
+  
+    // Connect interface elements to properties
+    teaName = (EditText) findViewById(R.id.tea_name);
+    brewTimeSeekBar = (SeekBar) findViewById(R.id.brew_time_seekbar);
+    brewTimeLabel = (TextView) findViewById(R.id.brew_time_value);
+    
+    // Setup Listeners
+    brewTimeSeekBar.setOnSeekBarChangeListener(this);
   }
+
+  /** Interface Methods **/
+  public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+    if(seekBar == brewTimeSeekBar) {
+      // Update the brew time label with the chosen value.
+      brewTimeLabel.setText((progress + 1) + " m");
+    }
+  }
+
+  public void onStartTrackingTouch(SeekBar seekBar) {}
+
+  public void onStopTrackingTouch(SeekBar seekBar) {}
 }
