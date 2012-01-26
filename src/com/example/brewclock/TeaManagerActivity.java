20afc0e24047bc05b47ac2bf70a32f00c415d0b4
Chris Blunt <chris@plymouthsoftware.com>
2012-01-26 08:24:12 +0000

    Added basic TeaManager activity.

---
+++ src/com/example/brewclock/TeaManagerActivity.java
@@ -0,0 +1,15 @@
+package com.example.brewclock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TeaManagerActivity extends Activity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  
+    setContentView(R.layout.tea_manager);
+  }
+  
+}
